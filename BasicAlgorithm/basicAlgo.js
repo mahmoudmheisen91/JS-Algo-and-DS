d7b92a6a5cc036021a5713cccab9e92901e50168
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-21 13:42:17 +0200

    add Find the Longest Word in a String algo

--- BasicAlgorithm/basicAlgo.js
+++ BasicAlgorithm/basicAlgo.js
@@ -59,3 +59,54 @@ function factorialize4(num, factorial = 1) {
 }
 
 factorialize(5);
+
+// Find the Longest Word in a String
+function findLongestWordLength(str) {
+  let arr = str.split(" ");
+  let max = 0;
+  for (let el of arr) {
+    if (el.length > max) {
+      max = el.length;
+      console.log(el);
+    }
+  }
+  return max;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+function findLongestWordLength2(s) {
+  return s.split(" ").reduce(function(x, y) {
+    return Math.max(x, y.length);
+  }, 0);
+}
+
+function findLongestWordLength3(str) {
+  return Math.max(...str.split(" ").map(word => word.length));
+}
+
+function findLongestWordLength(str) {
+  //split the string into individual words
+  //(important!!, you'll see why later)
+  str = str.split(" ");
+
+  //str only has 1 element left that is the longest element,
+  //return the length of that element
+  if (str.length == 1) {
+    return str[0].length;
+  }
+
+  //if the first element's length is greater than the second element's (or equal)
+  //remove the second element and recursively call the function)
+  if (str[0].length >= str[1].length) {
+    str.splice(1, 1);
+    return findLongestWordLength(str.join(" "));
+  }
+
+  //if the second element's length is greater thant the first element's start
+  //call the function past the first element
+  if (str[0].length <= str[1].length) {
+    // from the first element to the last element inclusive.
+    return findLongestWordLength(str.slice(1, str.length).join(" "));
+  }
+}
