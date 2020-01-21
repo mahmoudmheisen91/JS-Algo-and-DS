d378f5816f64c64e3d68396d41aa7bcb3532fd5b
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-21 13:28:30 +0200

    add Factorialize a Number algo

--- BasicAlgorithm/basicAlgo.js
+++ BasicAlgorithm/basicAlgo.js
@@ -23,3 +23,39 @@ function reverseString2(str) {
 }
 
 reverseString("hello");
+
+// Factorialize a Number
+function factorialize(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
+}
+
+// head
+function factorialize2(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
+
+//  Tail
+function factorialize3(num, factorial = 1) {
+  if (num == 0) {
+    return factorial;
+  } else {
+    return factorialize(num - 1, factorial * num);
+  }
+}
+
+function factorialize4(num, factorial = 1) {
+  return num < 0
+    ? 1
+    : new Array(num)
+        .fill(undefined)
+        .reduce((product, val, index) => product * (index + 1), 1);
+}
+
+factorialize(5);
