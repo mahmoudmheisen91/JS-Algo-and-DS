85c9e63723306bca9a7c086ac1ef3071a9d54312
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-21 13:18:03 +0200

    add Reverse a String function

--- BasicAlgorithm/basicAlgo.js
+++ BasicAlgorithm/basicAlgo.js
@@ -5,3 +5,21 @@ function convertToF(celsius) {
 }
 
 convertToF(30);
+
+// Reverse a String
+function reverseString(str) {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
+
+function reverseString2(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+reverseString("hello");
