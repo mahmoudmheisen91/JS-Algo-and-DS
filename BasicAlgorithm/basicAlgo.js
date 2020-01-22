3bdb23705a7f27362adcff58f429452c1515bb93
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-22 11:28:12 +0200

    add Title Case a Sentence algo

--- BasicAlgorithm/basicAlgo.js
+++ BasicAlgorithm/basicAlgo.js
@@ -275,10 +275,10 @@ function titleCase(str) {
 
 titleCase("I'm a little tea pot");
 
-function titleCase(str) {
-    var convertToArray = str.toLowerCase().split(" ");
-    var result = convertToArray.map(function(val) {
-      return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
-    });
-    return result.join(" ");
-  }
+function titleCase2(str) {
+  var convertToArray = str.toLowerCase().split(" ");
+  var result = convertToArray.map(function(val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return result.join(" ");
+}
