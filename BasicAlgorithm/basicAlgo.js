3c5fe87331434dd53acb11e4cc1ff8daf4025101
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-22 11:50:26 +0200

    add Slice and Splice func

--- BasicAlgorithm/basicAlgo.js
+++ BasicAlgorithm/basicAlgo.js
@@ -282,3 +282,23 @@ function titleCase2(str) {
   });
   return result.join(" ");
 }
+
+// Slice and Splice
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let arr3 = arr2.slice();
+  arr3.splice(n, 0, ...arr1);
+  return arr3;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+function frankenSplice2(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
