54519e0adfed09e8f4276e2cae7994c3abafcb85
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-20 18:47:17 +0200

    add solutions to Wherefore art thou algo

--- IntermediateAlgorithm/index.js
+++ IntermediateAlgorithm/index.js
@@ -111,3 +111,43 @@ console.log(whatIsInAName([{
     "apple": 1,
     "bat": 2
 }));
+
+// sol2:
+function whatIsInAName(collection, source) {
+    // "What's in a name? that which we call a rose
+    // By any other name would smell as sweet.”
+    // -- by William Shakespeare, Romeo and Juliet
+    var srcKeys = Object.keys(source);
+
+    return collection.filter(function (obj) {
+        return srcKeys.every(function (key) {
+            return obj.hasOwnProperty(key) && obj[key] === source[key];
+        });
+    });
+}
+
+// We start by filtering through collection using Array.filter().
+// Next, we map through all keys and return Boolean values based 
+// on the check conditions: both the key and its corresponding value 
+// must exist within the object we are filtering through.
+// Then we reduce the mapped Boolean values to a single Boolean that 
+// indicates whether all srcKeys pass the conditions checked above.
+// This single Boolean will be used to filter through the collection.
+
+function whatIsInAName(collection, source) {
+    // "What's in a name? that which we call a rose
+    // By any other name would smell as sweet.”
+    // -- by William Shakespeare, Romeo and Juliet
+    var srcKeys = Object.keys(source);
+
+    // filter the collection
+    return collection.filter(function (obj) {
+        return srcKeys
+            .map(function (key) {
+                return obj.hasOwnProperty(key) && obj[key] === source[key];
+            })
+            .reduce(function (a, b) {
+                return a && b;
+            });
+    });
+}
