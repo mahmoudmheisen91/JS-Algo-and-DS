907eb0f71d919c381b50bd3b7c6331712e8272ab
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-20 16:25:59 +0200

    add 2 solution to Diff Two Arrays algo

--- IntermediateAlgorithm/index.js
+++ IntermediateAlgorithm/index.js
@@ -42,3 +42,21 @@ function diffArray(arr1, arr2) {
 }
 
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+// sol 1:
+function diffArray(arr1, arr2) {
+    return arr1
+        .concat(arr2)
+        .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// sol 2:
+function diffArray(arr1, arr2) {
+    return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+    function diff(a, b) {
+        return a.filter(item => b.indexOf(item) === -1);
+    }
+}
