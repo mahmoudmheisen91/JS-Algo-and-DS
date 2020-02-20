72f079712713d51b49746a09e2e4fb8389c5117a
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-21 01:31:28 +0200

    add sols to pig latin algorithm

--- IntermediateAlgorithm/index.js
+++ IntermediateAlgorithm/index.js
@@ -209,3 +209,42 @@ console.log(translatePigLatin("california"));
 console.log(translatePigLatin("glove"));
 console.log(translatePigLatin("algorithm"));
 console.log(translatePigLatin("rhythm"));
+
+// sol1:
+function translatePigLatin(str) {
+    return str
+        .replace(/^[aeiou]\w*/, "$&way")
+        .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+}
+
+// sol2:
+function translatePigLatin(str) {
+    if (str.match(/^[aeiou]/)) return str + "way";
+
+    const consonantCluster = str.match(/^[^aeiou]+/)[0];
+    return str.substring(consonantCluster.length) + consonantCluster + "ay";
+}
+
+// sol3:
+function translatePigLatin(str) {
+    // Create variables to be used
+    var pigLatin = "";
+    var regex = /[aeiou]/gi;
+
+    // Check if the first character is a vowel
+    if (str[0].match(regex)) {
+        pigLatin = str + "way";
+    } else if (str.match(regex) === null) {
+        // Check if the string contains only consonants
+        pigLatin = str + "ay";
+    } else {
+        // Find how many consonants before the first vowel.
+        var vowelIndice = str.indexOf(str.match(regex)[0]);
+
+        // Take the string from the first vowel to the last char
+        // then add the consonants that were previously omitted and add the ending.
+        pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+    }
+
+    return pigLatin;
+}
