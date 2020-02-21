56b8e9e5b360c313fa166276fc6936183faef370
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-21 12:41:07 +0200

    add sols to map the depris algo
    foreach

--- IntermediateAlgorithm/index.js
+++ IntermediateAlgorithm/index.js
@@ -490,3 +490,44 @@ console.log(orbitalPeriod([{
     name: "moon",
     avgAlt: 378632.553
 }]));
+
+// sol 2:
+function orbitalPeriod(arr) {
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+
+    // Loop through each item in the array arr
+    arr.forEach(function (item) {
+        // Calculate the Orbital period value
+        var tmp = Math.round(
+            2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM)
+        );
+        //Delete the avgAlt property
+        delete item.avgAlt;
+        //Add orbitalPeriod property
+        item.orbitalPeriod = tmp;
+    });
+    return arr;
+}
+
+// sol 3:
+function orbitalPeriod(arr) {
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447;
+    return arr.map(({
+        name,
+        avgAlt
+    }) => {
+        const earth = earthRadius + avgAlt;
+        const orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earth, 3) / GM));
+        return {
+            name,
+            orbitalPeriod
+        };
+    });
+}
+
+orbitalPeriod([{
+    name: "sputnik",
+    avgAlt: 35873.5553
+}]);
