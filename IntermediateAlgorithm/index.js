4417f0e75e0237ef53f8ca040620a2a1022b30fc
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-21 02:44:40 +0200

    add sol to DNA pairing algo
    object instead of switch case

--- IntermediateAlgorithm/index.js
+++ IntermediateAlgorithm/index.js
@@ -401,3 +401,18 @@ function pairElement(str) {
 }
 
 console.log(pairElement("GCG"));
+
+// sol 1:
+function pairElement(str) {
+    //create object for pair lookup
+    var pairs = {
+        A: "T",
+        T: "A",
+        C: "G",
+        G: "C"
+    };
+    //split string into array of characters
+    var arr = str.split("");
+    //map character to array of character and matching pair
+    return arr.map(x => [x, pairs[x]]);
+}
