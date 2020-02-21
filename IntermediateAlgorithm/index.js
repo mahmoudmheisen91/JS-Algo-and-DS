eb7163c825ccd4233557eab54fc65a08a60b2156
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-21 02:18:17 +0200

    add sols to search and replace algorithm
    str.replace("s", "t")

--- IntermediateAlgorithm/index.js
+++ IntermediateAlgorithm/index.js
@@ -264,3 +264,107 @@ function myReplace(str, before, after) {
 const isUpperCase = (string) => /^[A-Z]/.test(string);
 
 console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
+
+// sol1
+function myReplace(str, before, after) {
+    // Find index where before is on string
+    var index = str.indexOf(before);
+    // Check to see if the first letter is uppercase or not
+    if (str[index] === str[index].toUpperCase()) {
+        // Change the after word to be capitalized before we use it.
+        after = after.charAt(0).toUpperCase() + after.slice(1);
+    }
+    // Now replace the original str with the edited one.
+    str = str.replace(before, after);
+
+    return str;
+}
+
+// sol2
+function myReplace(str, before, after) {
+    // Check if first character of argument "before" is a capital or lowercase letter and change the first character of argument "after" to match the case
+    if (/^[A-Z]/.test(before)) {
+        after = after[0].toUpperCase() + after.substr(1)
+    } else {
+        after = after[0].toLowerCase() + after.substr(1)
+    }
+
+    // return string with argument "before" replaced by argument "after" (with correct case)
+    return str.replace(before, after);
+}
+
+// sol 3:
+// Add new method to the String object, not overriding it if one exists already
+String.prototype.capitalize =
+    String.prototype.capitalize ||
+    function () {
+        return this[0].toUpperCase() + this.slice(1);
+    };
+
+const Util = (function () {
+    // Create utility module to hold helper functions
+    function textCase(str, tCase) {
+        // Depending if the tCase argument is passed we either set the case of the
+        // given string or we get it.
+        // Those functions can be expanded for other text cases.
+
+        if (tCase) {
+            return setCase(str, tCase);
+        } else {
+            return getCase(str);
+        }
+
+        function setCase(str, tCase) {
+            switch (tCase) {
+                case "uppercase":
+                    return str.toUpperCase();
+                case "lowercase":
+                    return str.toLowerCase();
+                case "capitalized":
+                    return str.capitalize();
+                default:
+                    return str;
+            }
+        }
+
+        function getCase(str) {
+            if (str === str.toUpperCase()) {
+                return "uppercase";
+            }
+            if (str === str.toLowerCase()) {
+                return "lowercase";
+            }
+            if (str === str.capitalize()) {
+                return "capitalized";
+            }
+            return "normal";
+        }
+    }
+
+    return {
+        textCase
+    };
+})();
+
+function myReplace(str, before, after) {
+    const {
+        textCase
+    } = Util;
+    const regex = new RegExp(before, "gi");
+    const replacingStr = textCase(after, textCase(before));
+
+    return str.replace(regex, replacingStr);
+}
+
+// sol4
+function myReplace(str, before, after) {
+    const myArr = str.split(" ");
+    const [wordToReplace] = myArr.filter(item => item === before);
+    return wordToReplace[0].toUpperCase() !== wordToReplace[0] ?
+        myArr.map(item => (item === before ? after : item)).join(" ") :
+        myArr
+        .map(item =>
+            item === before ? after[0].toUpperCase() + after.slice(1) : item
+        )
+        .join(" ");
+}
