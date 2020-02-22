bae1ee606136089499237f9b5ad918d84d2d5a76
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-22 20:06:07 +0200

    add sols to Arguments Optional algo
    some()

--- IntermediateAlgorithm/index.js
+++ IntermediateAlgorithm/index.js
@@ -862,3 +862,23 @@ function addTogether(...args) {
 }
 
 console.log(addTogether(2));
+
+// sol 2
+function addTogether(first, second) {
+    if (typeof first !== "number") {
+        return undefined;
+    }
+    const sum = second =>
+        typeof second === "number" ? first + second : undefined;
+    return typeof second === "undefined" ? second => sum(second) : sum(second);
+}
+
+// sol 3
+function addTogether() {
+    var args = Array.from(arguments);
+    return args.some(n => typeof n !== "number") ?
+        undefined :
+        args.length > 1 ?
+        args.reduce((acc, n) => (acc += n), 0) :
+        n => (typeof n === "number" ? n + args[0] : undefined);
+}
