7ef2aeea579e376b8af4b404556aa086749b182d
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-02-22 21:11:00 +0200

    add sols to missing letter algo
    diff

--- IntermediateAlgorithm/index.js
+++ IntermediateAlgorithm/index.js
@@ -933,3 +933,45 @@ function fearNotLetter(str) {
 }
 
 console.log(fearNotLetter("stvwx"));
+
+// sol 1
+function fearNotLetter(str) {
+    for (var i = 0; i < str.length; i++) {
+        /* code of current character */
+        var code = str.charCodeAt(i);
+
+        /* if code of current character is not equal to first character + no of iteration
+            hence character has been escaped */
+        if (code !== str.charCodeAt(0) + i) {
+            /* if current character has escaped one character find previous char and return */
+            return String.fromCharCode(code - 1);
+        }
+    }
+    return undefined;
+}
+
+// sol 2
+function fearNotLetter(str) {
+    var compare = str.charCodeAt(0),
+        missing;
+
+    str.split("").map(function (letter, index) {
+        if (str.charCodeAt(index) == compare) {
+            ++compare;
+        } else {
+            missing = String.fromCharCode(compare);
+        }
+    });
+
+    return missing;
+}
+
+
+// sol 3
+function fearNotLetter(str) {
+    for (let i = 1; i < str.length; ++i) {
+        if (str.charCodeAt(i) - str.charCodeAt(i - 1) > 1) {
+            return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+        }
+    }
+}
